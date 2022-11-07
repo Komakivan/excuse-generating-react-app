0570b6c652b44ae63792c5082440e7bef0e64d73
Komakivan <komakechivan555@gmail.com>
2022-11-07 20:59:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+
 
 function App() {
+
+  const [Excuse,setExcuse] = useState('')
+
+  const handleParty = () =>{
+    axios.get('https://excuser.herokuapp.com/v1/excuse/party/')
+    .then(res=>{
+      
+      setExcuse(res.data[0])
+    })
+  }
+
+  const handleFamily = () =>{
+    axios.get('https://excuser.herokuapp.com/v1/excuse/family/')
+    .then(res=>{
+      
+      setExcuse(res.data[0])
+    })
+  }
+
+  const handleOffice = () =>{
+    axios.get('https://excuser.herokuapp.com/v1/excuse/office/')
+    .then(res=>{
+      
+      setExcuse(res.data[0])
+    })
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Excuse generating machine</h1>
+      <div align='center'>
+        <Button variant='outline-success' onClick={handleParty}>Party</Button>
+        <Button variant='outline-success' onClick={handleFamily}>Family</Button>
+        <Button variant='outline-success' onClick={handleOffice}>Office</Button>
+        <br/>
+        <hr/>
+        <Card border="info" style={{ width: '18rem' }}>
+        <Card.Header>{Excuse.category} excuse for today</Card.Header>
+        <Card.Body>
+          <Card.Title>Excuse</Card.Title>
+          <Card.Text>
+            {Excuse.excuse}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div >
+      
     </div>
   );
 }
 
 export default App;
+
